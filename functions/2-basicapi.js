8e0d47f7d72fb0025db0de72bfc78e4cb33d7c5e
David Alaka <alakadavid05@gmail.com>
2021-03-18 15:50:31 +0100

    cors-fixed

--- functions/2-basicapi.js
+++ functions/2-basicapi.js
@@ -5,7 +5,7 @@ const array = require('../assets/data')
 exports.handler = async (event,context)=>{
 
     return {
-    
+    headers:{'Access-Control-Allow-Origin':'*'},
         statusCode:200,
         body:JSON.stringify(array)
     }
