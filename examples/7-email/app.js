b2b76bd9e1735c2f340a67aadbdbeb2c501d5459
David Alaka <alakadavid05@gmail.com>
2021-03-20 03:49:45 +0100

    smc

--- examples/7-email/app.js
+++ examples/7-email/app.js
@@ -7,3 +7,38 @@ const btn = document.querySelector('.submit-btn')
 const alert = document.querySelector('.alert')
 const title = document.querySelector('.title')
 alert.style.display = 'none'
+
+
+form.addEventListener("submit",async (e)=>{
+
+e.preventDefault();
+
+alert.style.display = "none";
+btn.disabled = true;
+btn.innerHTML = `<span class="sending"></span>`
+
+
+
+const name = nameInput.value 
+const email = emailInput.value 
+const subject = subjectInput.value 
+const message = messageInput.value 
+const data = {name,email,subject,message}
+try {
+    await axios.post('/api/7-email',{
+
+        data
+    })
+
+    nameInput.value = '';
+    emailInput.value = '';
+    subjectInput.value = '';
+    messageInput.value = '';
+} catch (error) {
+    console.log(error)
+    alert.style.display = "block";
+    alert.textContent = error.response.data
+}
+btn.disabled = false
+btn.innerHTML = "send"
+})
---
+++ functions/7-email.js
@@ -0,0 +1,78 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer')
+const {HOST,PORT,USER,PASS} = process.env
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    port:PORT,
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: USER,
+      pass: PASS
+    }
+  });
+
+exports.handler = async (event,context)=>{
+
+
+    // const method =event.httpMethod;
+   /*  if (method !== POST){
+
+        return {
+            headers:{'Access-Control-Allow-Origin':'*'},
+                statusCode:405,
+                body:'only post request is allowed'
+            }
+
+    } */
+
+    // const {name,email,message,subject} = event.body
+    console.log(JSON.parse(event.body),'x')
+const res = JSON.parse(event.body)
+const {name,email,message,subject} = res.data
+    if(!name || !email||!message|| !subject){
+
+        return {
+            headers:{'Access-Control-Allow-Origin':'*'},
+                statusCode:400,
+                body:'provide all values'
+            }
+    }
+    
+
+
+const data = {
+
+from: `darcoders77@gmail.com`,
+
+to:"dolapoajayi28@gmail.com",
+
+subject: "pass",
+
+html:`<p>${message}</p>`
+
+}
+
+try {
+    
+    await transporter.sendMail({...data})
+    return {
+        headers:{'Access-Control-Allow-Origin':'*'},
+            statusCode:200,
+            body:'success'
+        }
+
+
+} catch (error) {
+    
+    return {
+        headers:{'Access-Control-Allow-Origin':'*'},
+            statusCode:400,
+            body:'error'
+        }
+}
+
+
+
+
+
+    }
